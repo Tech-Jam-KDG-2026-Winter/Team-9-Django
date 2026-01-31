acd30cb22899b18dbdeb49a3a880110d1efe744e
shiho2717 <kkc.sh.2717@gmail.com>
2026-01-31 17:26:39 +0900

    ReservationのFK用仮Teamモデル追加

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, default="temp")
+
+    def __str__(self):
+        return self.name
