6b6c14432b84cba00b6c7d63b5635f3b25968e91
shiho2717 <kkc.sh.2717@gmail.com>
2026-02-01 19:18:54 +0900

    予約作成フォームを作成

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ReservationForm
 
+
+@login_required
 def new_reservation(request):
-    return render(request, "reservations/new.html")
-# Create your views here.
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            return redirect("dashboard")
+    else:
+        form = ReservationForm()
+
+    return render(request, "reservations/new.html", {"form": form})
+
+
+def dashboard(request):
+    return render(request, "dashboard.html")
